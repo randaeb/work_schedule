7a7178b7b38a3980d5de36a7996b4f8ed5729e5a
Randa Bukowski <randabukowski@Randas-MacBook-Pro.local>
2022-08-16 14:19:04 -0700

    html

---
+++ script.js
@@ -0,0 +1,29 @@
+//Make variables
+var currentDay
+var hour 
+var checkHour
+
+//use jQuery DOM
+
+//create a save button 
+
+
+
+// save in local storage api 
+
+
+//if else statements?
+
+
+//create setInterval 
+
+//create my time blocks 
+
+
+//create event listeners for my the time blocks
+
+
+//color coded time blocks for past present and future?
+
+
+// 
